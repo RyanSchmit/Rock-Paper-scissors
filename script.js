09e750147be6b238f980825262c1f6335ce8e1fe
Ryan Sch <ryan@Ryans-MacBook-Air.local>
2022-04-09 21:42:22 -0700

    Added semi-colons

--- script.js
+++ script.js
@@ -3,51 +3,51 @@ var computerChoice;
 var userChoice;
 
 function computerPlay() {
-	computerChoice = choices[Math.floor(Math.random() * 3)]
+	computerChoice = choices[Math.floor(Math.random() * 3)];
 }
 
 function userPlay() {
-	userChoice = prompt("Choice rock, paper, or scissors").trim().toLowerCase()
+	userChoice = prompt("Choice rock, paper, or scissors").trim().toLowerCase();
 	while (true) {
 		if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
-			break
+			break;
 		} else {
-			userPlay()
+			userPlay();
 		}
 	}
 }
 
 function playRound(playerSelection, computerSelection) {
-	let losingMessage = "You lost! " + computerChoice + " beats " + userChoice
-	let winningMessage = "You won! " + userChoice + " beats " + computerChoice
-	let tieMessage = "It's a tie! You both choose " + userChoice
+	let losingMessage = "You lost! " + computerChoice + " beats " + userChoice;
+	let winningMessage = "You won! " + userChoice + " beats " + computerChoice;
+	let tieMessage = "It's a tie! You both choose " + userChoice;
 	if (userChoice == computerChoice) {
-		return(tieMessage)
+		return(tieMessage);
 	}
 	else if (userChoice == "rock" && computerChoice == "paper") {
-		return(losingMessage)
+		return(losingMessage);
 	}
 	else if (userChoice == "rock" && computerChoice == "scissors") {
-		return(winningMessage)
+		return(winningMessage);
 	}
 	else if (userChoice == "paper" && computerChoice == "scissors") {
-		return(losingMessage)
+		return(losingMessage);
 	}
 	else if (userChoice == "paper" && computerChoice == "rock") {
-		return(winningMessage)
+		return(winningMessage);
 	}
 	else if (userChoice == "scissors" && computerChoice == "rock") {
-		return(losingMessage)
+		return(losingMessage);
 	}
 	else if (userChoice == "scissors" && computerChoice == "paper") {
-		return(winningMessage)
+		return(winningMessage);
 	}
 }
 
 function game() {
-	userPlay()
-	computerPlay()
-	console.log(playRound(userChoice, computerChoice))
+	userPlay();
+	computerPlay();
+	console.log(playRound(userChoice, computerChoice));
 }
 
-game()
+game();
