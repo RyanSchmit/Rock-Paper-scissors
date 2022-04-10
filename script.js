d64d576d7821634ecd22253ed077d72fc7f0f10e
Ryan Sch <ryan@Ryans-MacBook-Air.local>
2022-04-09 19:28:33 -0700

    Changed functions and user input

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
-let choices = ['rock', 'paper', 'scissors']
-let userChoice = prompt("Choice rock, paper, or scissors").trim().toLowerCase()
-let computerChoice = choices[Math.floor(Math.random() * 3)]
+let choices = ['rock', 'paper', 'scissors'];
+var computerChoice;
+var userChoice;
 
-if (userChoice == computerChoice) {
-	console.log("It's a tie")
-}
-else if (userChoice == "rock" && computerChoice == "paper") {
-	console.log("You lost")
-}
-else if (userChoice == "rock" && computerChoice == "scissors") {
-	console.log("You won")
-}
-else if (userChoice == "paper" && computerChoice == "scissors") {
-	console.log("You lost")
-}
-else if (userChoice == "paper" && computerChoice == "rock") {
-	console.log("You won")
-}
-else if (userChoice == "scissors" && computerChoice == "rock") {
-	console.log("You lost")
-}
-else if (userChoice == "scissors" && computerChoice == "paper") {
-	console.log("You won")
+function computerPlay() {
+	computerChoice = choices[Math.floor(Math.random() * 3)]
 }
 
-console.log("You choose " + userChoice)
-console.log("The computer choose " + computerChoice)
+function userPlay() {
+	userChoice = prompt("Choice rock, paper, or scissors").trim().toLowerCase()
+	while (true) {
+		if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+			break
+		} else {
+			userPlay()
+		}
+	}
+}
+
+function playRound(playerSelection, computerSelection) {
+	let losingMessage = "You lost! " + computerChoice + " beats " + userChoice
+	let winningMessage = "You won! " + userChoice + " beats " + computerChoice
+	let tieMessage = "It's a tie! You both choose " + userChoice
+	if (userChoice == computerChoice) {
+		return(tieMessage)
+	}
+	else if (userChoice == "rock" && computerChoice == "paper") {
+		return(losingMessage)
+	}
+	else if (userChoice == "rock" && computerChoice == "scissors") {
+		return(winningMessage)
+	}
+	else if (userChoice == "paper" && computerChoice == "scissors") {
+		return(losingMessage)
+	}
+	else if (userChoice == "paper" && computerChoice == "rock") {
+		return(winningMessage)
+	}
+	else if (userChoice == "scissors" && computerChoice == "rock") {
+		return(losingMessage)
+	}
+	else if (userChoice == "scissors" && computerChoice == "paper") {
+		return(winningMessage)
+	}
+}
+
+function game() {
+	userPlay()
+	computerPlay()
+	console.log(playRound(userChoice, computerChoice))
+}
+
+game()
