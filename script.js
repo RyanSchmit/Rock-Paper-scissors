c9b6ef82d4c22f0015a10ee8a6648d41890a2137
Ryan Sch <ryan@Ryans-Air.home>
2022-06-07 15:37:38 -0400

    Added UI

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let computerChoice;
 let userChoice;
 let choices = ["rock", "paper", "scissors"]
+let result;
+
+function reset() {
+	let images = document.querySelectorAll(".computer");
+	images.forEach((image) => {
+		image.style.backgroundColor = "white";
+	})
+}
 
 function computerPlay() {
+	reset();
 	computerChoice = choices[Math.floor(Math.random() * 3)];
+	let image = document.querySelector(".computer." + computerChoice);
+	image.style.backgroundColor = "red";
 }
 
 function playRound() {
@@ -13,33 +24,46 @@ function playRound() {
 	if (userChoice == computerChoice) {
 		return(tieMessage);
 	}
-	else if (userChoice == "rock" && computerChoice == "paper") {
-		return(losingMessage);
-	}
 	else if (userChoice == "rock" && computerChoice == "scissors") {
 		return(winningMessage);
-	}
-	else if (userChoice == "paper" && computerChoice == "scissors") {
-		return(losingMessage);
+		result = 'win';
+		alert(result);
 	}
 	else if (userChoice == "paper" && computerChoice == "rock") {
 		return(winningMessage);
-	}
-	else if (userChoice == "scissors" && computerChoice == "rock") {
-		return(losingMessage);
+		result = 'win';
+		alert(result);
 	}
 	else if (userChoice == "scissors" && computerChoice == "paper") {
 		return(winningMessage);
+		result = 'win';
+		alert(result);
+	}
+	else if (userChoice == "rock" && computerChoice == "paper") {
+		return(losingMessage);
+		result = 'lose'
+		alert(result);
+	}
+	else if (userChoice == "paper" && computerChoice == "scissors") {
+		return(losingMessage);
+		result = 'lose'
+		alert(result);
+	}
+	else if (userChoice == "scissors" && computerChoice == "rock") {
+		return(losingMessage);
+		result = 'lose'
+		alert(result);
 	}
 }
 
 function game() {
 	computerPlay();
 	userChoice = this.id;
-	alert(playRound());
+	let resultPlaceHolder = document.getElementById("result");       
+	resultPlaceHolder.textContent = playRound();
 }
 
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("a");
 buttons.forEach((button) => {
 	button.addEventListener('click', game)
 })
